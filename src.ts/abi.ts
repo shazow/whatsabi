108af901b839ee425442880d848fefdc897c08bd
Andrey Petrov <andrey.petrov@shazow.net>
2022-10-05 09:51:47 -0700

    abi: nameAlts

--- src.ts/abi.ts
+++ src.ts/abi.ts
@@ -1,7 +1,7 @@
 export interface ABIFunction {
     type: "function";
     name?: string;
-    nameCandidates?: string[];
+    nameAlts?: string[];
     selector: string;
     payable?: boolean;
     // TODO: constant: boolean;
@@ -12,7 +12,7 @@ export interface ABIFunction {
 export interface ABIEvent {
     type: "event",
     name?: string;
-    nameCandidates?: string[];
+    nameAlts?: string[];
     hash: string,
     // TODO: ...
 };
