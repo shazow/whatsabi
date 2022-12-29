d57470c8f1bd25c4cf4962ab47f23afe7ec7a01f
Andrey Petrov <andrey.petrov@shazow.net>
2022-12-29 11:49:15 -0500

    tests: Add caching to selectors tests

--- src.ts/__tests__/offline.ts
+++ src.ts/__tests__/offline.ts
@@ -1,30 +1,33 @@
 import * as fs from 'fs';
 import { test } from '@jest/globals';
 
-const FIXTURES_DIR = "__fixtures__"
+const CACHE_DIR = ".cache"
 
 // Skip online tests unless ONLINE env is set
 export const online_test = process.env["ONLINE"] !== undefined ? test : test.skip;
-export const cached_test = process.env["CACHED"] ? test : test.skip;
+export const cached_test = !process.env["SKIP_CACHED"] ? test : test.skip;
 
 export async function withCache(
   cacheKey: string,
   cacheGetter: () => Promise<any>,
 ): Promise<any> {
   // Check if cache exists
-  const cachePath = `${FIXTURES_DIR}/${encodeURIComponent(cacheKey)}`;
+  const cachePath = `${CACHE_DIR}/${encodeURIComponent(cacheKey)}`;
   if (fs.existsSync(cachePath)) {
+    console.debug("withCache: Using cached value:", cachePath);
     const val = JSON.parse(fs.readFileSync(cachePath, 'utf8'));
     return val;
   }
 
   const val = await cacheGetter();
+  if (val === undefined) throw "withCache: undefined value from cacheGetter";
 
   // Save it
-  if (!fs.existsSync(FIXTURES_DIR)) {
-    fs.mkdirSync(FIXTURES_DIR);
+  if (!fs.existsSync(CACHE_DIR)) {
+    fs.mkdirSync(CACHE_DIR);
   }
 
+  console.debug("withCache: Saving test cache:", cachePath);
   fs.writeFileSync(cachePath, JSON.stringify(val));
 
   return val;
