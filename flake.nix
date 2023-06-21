e9d5ee152e15dd12d68712cd5f4d0d89fd9ae695
Andrey Petrov <andrey.petrov@shazow.net>
2023-06-21 15:24:19 -0400

    .github: Update workflow

--- flake.nix
+++ flake.nix
@@ -14,15 +14,15 @@
         buildInputs = [
           pkgs.nodejs-18_x
 
-          pkgs.nodePackages.typescript
-          pkgs.nodePackages.typescript-language-server
-
           pkgs.graphviz  # For debugging
+          pkgs.gnumake
         ];
 
         shellHook = ''
           export PS1="[dev] $PS1"
           export PATH=$PWD/node_modules/.bin:$PATH
+
+          [[ ! -d node_modules ]] && npm install
 
           [[ -f .env ]] && source .env
         '';
