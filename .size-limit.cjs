022fdb189788e501fe9aca7f161a206a80e98b1a
Andrey Petrov <andrey.petrov@shazow.net>
2023-09-18 18:16:41 -0700

    package: Tweak tsconfig, add package.json stubs

--- .size-limit.cjs
+++ .size-limit.cjs
@@ -3,5 +3,10 @@ module.exports = [
     path: "lib.esm/index.js",
     import: "{ whatsabi }",
     limit: "20 kb"
+  },
+  {
+    path: "lib.cjs/index.js",
+    import: "{ whatsabi }",
+    limit: "150 kb"
   }
 ]
