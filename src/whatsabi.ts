0f3ce410d9fa9298741a5159d83ad7340003f13a
Andrey Petrov <andrey.petrov@shazow.net>
2024-08-09 17:17:50 -0400

    src/whatsabi.ts: Export errors

--- src/whatsabi.ts
+++ src/whatsabi.ts
@@ -21,3 +21,6 @@ import * as loaders from "./loaders.js";
 import * as proxies from "./proxies.js";
 export { loaders };
 export { proxies };
+
+import * as errors from "./errors.js";
+export { errors };
