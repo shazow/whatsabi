4c1e0f8df7bd425cfcf5dc07ce5100541d73d1e7
Andrey Petrov <andrey.petrov@shazow.net>
2024-09-11 14:15:01 -0700

    src/__tests__: Add autoload crossload tests

--- src/__tests__/env.ts
+++ src/__tests__/env.ts
@@ -5,7 +5,7 @@ import { createPublicClient, createWalletClient, http } from 'viem';
 import { Web3 } from "web3";
 
 import { withCache } from "../internal/filecache";
-import { CompatibleProvider } from "../providers.js";
+import { CompatibleProvider, Provider } from "../providers.js";
 
 const env = {
     INFURA_API_KEY: process.env.INFURA_API_KEY,
@@ -17,31 +17,42 @@ const env = {
 
 const DEFAULT_PUBLIC_RPC = "https://ethereum-rpc.publicnode.com";
 
-const provider = CompatibleProvider(function() {
-    let rpc_url = env.PROVIDER_RPC_URL;
-    if (env.INFURA_API_KEY) {
-        rpc_url = "https://mainnet.infura.io/v3/" + env.INFURA_API_KEY;
-    }
 
-    if (env.PROVIDER?.startsWith("viem")) {
-        const transport = http(rpc_url ?? DEFAULT_PUBLIC_RPC);
-        if (env.PROVIDER.endsWith("publicClient")) return createPublicClient({ transport });
-        if (env.PROVIDER.endsWith("transport")) return transport({});
-        return createWalletClient({ transport });
-    }
+/**
+ * Make a CompatibleProvider for testing relative to the environment we're testing against.
+ */
+export function makeProvider(rpc_url?: string): Provider {
+    return CompatibleProvider(function() {
+        if (!rpc_url) {
+            if (env.INFURA_API_KEY) {
+                rpc_url = "https://mainnet.infura.io/v3/" + env.INFURA_API_KEY;
+            } else {
+                rpc_url = env.PROVIDER_RPC_URL;
+            }
+        }
 
-    if (env.PROVIDER === "web3") {
-        return new Web3(rpc_url ?? DEFAULT_PUBLIC_RPC);
-    }
+        if (env.PROVIDER?.startsWith("viem")) {
+            const transport = http(rpc_url ?? DEFAULT_PUBLIC_RPC);
+            if (env.PROVIDER.endsWith("publicClient")) return createPublicClient({ transport });
+            if (env.PROVIDER.endsWith("transport")) return transport({});
+            return createWalletClient({ transport });
+        }
 
-    if (!env.PROVIDER || env.PROVIDER === "ethers") {
-        if (env.PROVIDER_RPC_URL) return new ethers.JsonRpcProvider(env.PROVIDER_RPC_URL);
-        if (env.INFURA_API_KEY) return new ethers.InfuraProvider("homestead", env.INFURA_API_KEY);
-        return new ethers.JsonRpcProvider(DEFAULT_PUBLIC_RPC);
-    }
+        if (env.PROVIDER === "web3") {
+            return new Web3(rpc_url ?? DEFAULT_PUBLIC_RPC);
+        }
 
-    throw new Error("Unknown PROVIDER: " + env.PROVIDER);
-}());
+        if (!env.PROVIDER || env.PROVIDER === "ethers") {
+            if (rpc_url) return new ethers.JsonRpcProvider(rpc_url);
+            if (env.INFURA_API_KEY) return new ethers.InfuraProvider("homestead", env.INFURA_API_KEY);
+            return new ethers.JsonRpcProvider(DEFAULT_PUBLIC_RPC);
+        }
+
+        throw new Error("Unknown PROVIDER: " + env.PROVIDER);
+    }());
+}
+
+const provider = makeProvider();
 
 type ItConcurrent = typeof test.skip;
 
