d37a843bfcdc2ab2794a468e079b2d6a07dc3c0e
Andrey Petrov <andrey.petrov@shazow.net>
2024-10-01 14:18:34 -0400

    flake: use latest node, update

--- flake.nix
+++ flake.nix
@@ -1,8 +1,11 @@
 {
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
   outputs = {
-    self,
     nixpkgs,
     flake-utils,
+    ...
   }:
     flake-utils.lib.eachDefaultSystem
     (system: let
@@ -11,10 +14,11 @@
       };
     in {
       devShells.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.nodejs_22
-          pkgs.nodePackages.pnpm
+        buildInputs = with pkgs.nodePackages_latest; [
+          nodejs
+          pnpm
 
+        ] ++ [
           pkgs.graphviz  # For debugging
           pkgs.gnumake
         ];
