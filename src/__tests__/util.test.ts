49bf3eda9e3a96aa584de6dff209777ca43f36fc
Andrey Petrov <andrey.petrov@shazow.net>
2024-10-10 18:37:00 -0400

    src/disasm.ts: Use last 20 bytes of push value for detected FixedProxyResolver
    Fixes #139

--- src/__tests__/util.test.ts
+++ src/__tests__/util.test.ts
@@ -6,18 +6,18 @@ import { hexToBytes, bytesToHex, keccak256 } from "../utils";
 
 describe('Utils', () => {
   test.each([
-    new Uint8Array([0,1,2,3]),
-    new Uint8Array([42,69,255]),
+    new Uint8Array([0, 1, 2, 3]),
+    new Uint8Array([42, 69, 255]),
     new Uint8Array([255]),
-    new Uint8Array([255,255]),
-    new Uint8Array([0,255,0,255]),
+    new Uint8Array([255, 255]),
+    new Uint8Array([0, 255, 0, 255]),
   ])("bytesToHex %s", (bytes) => {
     expect(bytesToHex(bytes)).toStrictEqual(ethers.hexlify(bytes));
   });
 
   test("bytesToHex padding", () => {
     expect(bytesToHex(new Uint8Array([0]), 20)).toStrictEqual("0x0000000000000000000000000000000000000000");
-    expect(bytesToHex(new Uint8Array([255,255,255]), 20)).toStrictEqual("0x0000000000000000000000000000000000ffffff");
+    expect(bytesToHex(new Uint8Array([255, 255, 255]), 20)).toStrictEqual("0x0000000000000000000000000000000000ffffff");
   });
 
 
@@ -36,8 +36,8 @@ describe('Utils', () => {
     "0x00010203",
     "0xffff",
     "0xffff0000111122223333444455556666777788889999aaaabbbbccccddddeeee",
-    new Uint8Array([0,1,2,3]),
-    new Uint8Array([255,0,255,0,255,0]),
+    new Uint8Array([0, 1, 2, 3]),
+    new Uint8Array([255, 0, 255, 0, 255, 0]),
   ])("keccak256 %s", (hex) => {
     expect(keccak256(hex)).toStrictEqual(ethers.keccak256(hex));
   });
